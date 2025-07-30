4afd031f9612ab4bfb631e35ebf369c21de84123
Alexandre Amorim <amorimalexandre2005@gmail.com>
2025-07-30 02:40:38 +0000

    pelo fato da api do whatsapp estar um pouco desatualizada esse erro não

--- src/ai/flows/whatsapp-flow.ts
+++ src/ai/flows/whatsapp-flow.ts
@@ -50,6 +50,10 @@ const generateQrCodeFlow = ai.defineFlow(
           headless: true,
           args: ['--no-sandbox', '--disable-setuid-sandbox'],
         },
+        webVersionCache: {
+          type: 'remote',
+          remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+        },
       });
 
       const timeout = setTimeout(() => {
