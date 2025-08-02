6e4bf0e7025a65c3fea62be4b9461408bfff8d32
Alexandre Amorim <amorimalexandre2005@gmail.com>
2025-08-02 23:06:31 +0000

    Modifique a inicialização do seu Client: Você precisará remover o execut

--- src/ai/flows/whatsapp-flow.ts
+++ src/ai/flows/whatsapp-flow.ts
@@ -78,12 +78,6 @@ const generateQrCodeFlow = ai.defineFlow(
           args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process', // <- this one doesn't works in Windows
-            '--disable-gpu'
           ],
         },
       });
